064101b588e16cb035b8457f814cbdb0c4c56d3c
Newton Lomar <40671286+newlomar@users.noreply.github.com>
2024-10-10 00:20:55 -0300

    migrando para web HID

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,56 +17,28 @@ export default function ConnectBluetoothComponent() {
 
   const connectToDevice = async () => {
     try {
-      const device = await navigator.hid.requestDevice({
+      const requestDevice = await navigator.hid.requestDevice({
         filters: [],
       });
 
       alert();
 
-      setDevice(device[0]);
-      console.log(`Device Name: ${device.name}`);
-
-      const server = await device.gatt?.connect();
-
-      if (!server) {
-        throw new Error("GATT server not found");
+      if (!requestDevice[0]) {
+        throw new Error("No Device 1");
       }
 
-      console.log("Connected to GATT server");
+      setDevice(requestDevice[0]);
+
+      if (!device) {
+        throw new Error("No Device 2");
+      }
+
       setIsConnected(true);
+      alert(`Device Name: ${device.productName}`);
 
-      const services = await server.getPrimaryServices();
-      services.forEach((service) => {
-        alert(service.uuid);
-      });
-
-      const service = await server.getPrimaryService(
-        "00001812-0000-1000-8000-00805f9b34fb"
-      );
-
-      if (!service) {
-        throw new Error("Error getting service");
-      }
-
-      alert(JSON.stringify(service));
-
-      const characteristic = await service.getCharacteristic(
-        "00002a4d-0000-1000-8000-00805f9b34fb"
-      );
-
-      const batteryLevelBinary = await characteristic.readValue();
-
-      const batteryLevelHumanReadable = batteryLevelBinary.getInt8(0);
-
-      alert(batteryLevelHumanReadable);
-
-      await characteristic.startNotifications();
-      characteristic.addEventListener(
-        "characteristicvaluechanged",
-        handleButtonPress
-      );
+      device.addEventListener("inputreport", handleButtonPress);
     } catch (err) {
-      console.error("Error connecting to Bluetooth device: ", err);
+      console.error("Error connecting to HID device: ", err);
       setError((err as Error).message);
     }
   };
@@ -74,25 +46,27 @@ export default function ConnectBluetoothComponent() {
   const disconnectToDevice = async () => {
     try {
       if (device && isConnected) {
-        device.gatt?.disconnect();
+        device.close();
         setDevice(null);
         setIsConnected(false);
       }
     } catch (err) {
-      console.error("Error trying to diconnect to Bluetooth device: ", err);
+      console.error("Error trying to diconnect to HID device: ", err);
       setError((err as Error).message);
     }
   };
 
-  const handleButtonPress = (event: Event) => {
-    const target = event.target as BluetoothRemoteGATTCharacteristic;
-    const value = target.value;
+  const handleButtonPress = (event: HIDInputReportEvent) => {
+    const { data } = event;
+
+    const value = data.getUint8(0);
+
+    if (value === 0) {
+      alert("value === 0");
+      return;
+    }
 
     alert(`Botão pressionado: ${value}`);
-
-    if (value?.getUint8(0)) {
-      alert("Click de botão foi ouvido!");
-    }
   };
 
   return (
@@ -107,7 +81,7 @@ export default function ConnectBluetoothComponent() {
     >
       <h1>Bluetooth Device Connection</h1>
       {device ? (
-        <p>Connected to: {device.name}</p>
+        <p>Connected to: {device.productName}</p>
       ) : (
         <p>No Bluetooth device connected.</p>
       )}
